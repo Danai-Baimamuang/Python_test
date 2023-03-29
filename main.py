d447c4be3261877d028809aca3bba54ea991a089
Danai-Baimamuang <PkZnc2GP@enauka.edu.pl>
2023-03-29 22:27:12 +0000

    Auto-format code เรียบร้อย [✅🫡]

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ e = 4
 def main():
     print("hello")
 
-class student():
- def __init__(self):
-    self.name = "danai"
+
+class student:
+    def __init__(self):
+        self.name = "danai"
+
 
 st = student().name
 print(st)
